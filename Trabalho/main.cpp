2a2bba0d05222d021df86252063dcf2134209321
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2019-10-27 10:42:21 -0300

    Entradas novas adicionadas, modificaçoes no makefile e fechamento de arquivos na main

--- Trabalho/main.cpp
+++ Trabalho/main.cpp
@@ -17,15 +17,12 @@ int main(int argc, char** argv){
     }
     for (int i = 1; i < argc; i++){
         flags.push_back(string(argv[i]));
-        cout << argv[i] << endl;
     }
     ifstream generos, usuarios, favoritos, midias;
     for (int i = 0; i < flags.size() - 1; i++){
         //Genero
         if (flags[i] == "-g"){
-            cout << "Genero" << endl;
             generos.open(flags[i+1]);
-            cout << flags[i+1] << endl;
             if(generos.fail()){
                 cerr << "Erro de I/O"<< endl;
                 exit(1);
@@ -34,8 +31,6 @@ int main(int argc, char** argv){
         }
         //Usuarios
         if (flags[i] == "-u"){
-            cout << "Usuarios" << endl;
-            cout << flags[i+1] << endl;
             usuarios.open(flags[i+1]);
             if(usuarios.fail()){
                 cerr << "Erro de I/O"<< endl;
@@ -45,8 +40,6 @@ int main(int argc, char** argv){
         }
         //Favoritos
         if (flags[i] == "-f"){
-            cout << "Favoritos" << endl;
-            cout << flags[i+1] << endl;
             favoritos.open(flags[i+1]);
             if(favoritos.fail()){
                 cerr << "Erro de I/O"<< endl;
@@ -56,8 +49,6 @@ int main(int argc, char** argv){
         }
         //Midias
         if (flags[i] == "-m"){
-            cout << "Midias" << endl;
-            cout << flags[i+1] << endl;
             midias.open(flags[i+1]);
             if(midias.fail()){
                 cerr << "Erro de I/O"<< endl;
@@ -68,30 +59,12 @@ int main(int argc, char** argv){
     }
     ofstream saida, saida2;
     spotify->carregaArquivoGeneros(generos);
+    generos.close();
     spotify->carregaArquivoUsuarios(usuarios);
+    usuarios.close();
     spotify->carregaArquivoMidia(midias);
+    midias.close();
     spotify->carregaArquivoFavoritos(favoritos);
+    favoritos.close();
     spotify->gerarRelatorios();
-
-    // cout << endl;
-    
-    // spotify->imprimeAssinantes();
-    // cout << "====================Artistas" << endl;
-    // spotify->imprimeArtistas();
-    // cout << "====================Podcasters" << endl;
-    // spotify->imprimePodcasters();
-    // saida.open("saida.txt");
-    // saida2.open("saida2.txt");
-    // for(Genero* g : spotify->getgenero()){
-    //     saida << g->getsigla() << ";";
-    //     saida << g->getnome() << endl;
-    // }
-    // cout << "===========---------------Midias" << endl;
-    // spotify->imprimeMidias();
-
-    // for(Usuario* u : spotify->getusuario()){
-    //     u->imprimeNoArquivo(saida2);
-    //     // saida2 << u->getcodigo() << ";";
-    //     // saida2 << u->getnome() << endl;
-    // }
 }
